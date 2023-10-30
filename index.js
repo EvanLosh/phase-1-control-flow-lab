cee8d05c2afe3cecef3e4ed6d2e362274aa44a3d
Evan Losh <evand.losh@gmail.com>
2023-10-30 15:51:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceFeet) {
   // Write your code here!
+  if (distanceFeet < 400) {
+    return "This one is on me!";
+  }
+  if ((distanceFeet > 400) && (distanceFeet < 2000)) {
+    return "That will be twenty bucks.";
+  }
+  if ((distanceFeet > 2000) && (distanceFeet < 2500)) {
+    return "I will gladly take your thirty bucks.";
+  }
+  if (distanceFeet > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination) {
   // Write your code here!
+  if (destination == "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous': return "Thank you so much.";
+    case 'not as generous': return "Thank you.";
+    default: return 'Bye.';
+  }
 }
